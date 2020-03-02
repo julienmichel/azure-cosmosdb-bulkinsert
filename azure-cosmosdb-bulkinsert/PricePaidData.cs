b1b49da7ec2f24cc3934e8fb6b1e281e3c5554b4
Julien Michel <julien_michel@hotmail.com>
2020-03-02 15:38:09 +0100

    Add files via upload

---
+++ azure-cosmosdb-bulkinsert/PricePaidData.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace azure_cosmosdb_bulkinsert
+{
+    public class PricePaidData
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Transaction_unique_identifieroperty { get; set; }
+        public string Price { get; set; }
+
+        public string Date_of_Transfer { get; set; }
+        public string Postcode { get; set; }
+        public string PropertyType { get; set; }
+        public string isNew { get; set; }
+        public string Duration { get; set; }
+        public string PAON { get; set; }
+        public string SAON { get; set; }
+        public string Street { get; set; }
+        public string Locality { get; set; }
+        public string Town_City { get; set; }
+
+        public string District { get; set; }
+        public string County { get; set; }
+        public string PPD_Category { get; set; }
+        public string Record_Status { get; set; }
+        public string pk { get; set; }
+    }
+}
+
+
